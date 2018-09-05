e3deb7d32894518dac41679789a3d92697745b06
yangyi02 <yangyi02@gmail.com>
2018-09-05 14:00:46 -0700

    add set_env.sh

---
+++ kitti_det/set_env.sh
@@ -0,0 +1,8 @@
+mkdir -p cache/logs
+mkdir -p cache/figures
+mkdir -p cache/models
+mkdir -p cache/tensorboard
+mkdir -p cache/tests/logs
+mkdir -p cache/tests/figures
+mkdir -p cache/tests/models
+mkdir -p cache/tests/tensorboard
