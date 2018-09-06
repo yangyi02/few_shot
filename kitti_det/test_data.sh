4cb6f16ee477897be94435d6aea1939a3e289a8f
yangyi02 <yangyi02@gmail.com>
2018-09-06 12:01:54 -0700

    fix bug

--- kitti_det/test_data.sh
+++ kitti_det/test_data.sh
@@ -7,5 +7,5 @@ DEPTH_NAME=$DATA_PATH/disp_unsup/000003.png
 FLOW_NAME=$DATA_PATH/flow_unsup/000003.png
 BOX_NAME=$DATA_PATH/label_2/000003.txt
 
-python $CODE_PATH/test_data.py --data_path=$KITTI_DATA_PATH --image_name=$IMAGE_NAME --depth_name=$DEPTH_NAME --flow_name=$FLOW_NAME --box_name=$BOX_NAME
+python $CODE_PATH/test_data.py --data_path=$DATA_PATH --image_name=$IMAGE_NAME --depth_name=$DEPTH_NAME --flow_name=$FLOW_NAME --box_name=$BOX_NAME
 
