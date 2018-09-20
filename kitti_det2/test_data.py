fcab9bddb3bffe816c034795305c2aeb267f8d25
yangyi02 <yangyi02@gmail.com>
2018-09-19 17:28:37 -0700

    change back test_data

--- kitti_det2/test_data.py
+++ kitti_det2/test_data.py
@@ -21,27 +21,27 @@ class DataTest(object):
 def main():
     args = parse_args()
     logging.info(args)
-    args.data_path = '/media/yi/DATA/data-orig/kitti/training'
-    args.image_name = '/media/yi/DATA/data-orig/kitti/training/image_2/007480.png'
-    args.depth_name = '/media/yi/DATA/data-orig/kitti/training/disp_unsup/007480.png'
-    args.flow_name = '/media/yi/DATA/data-orig/kitti/training/flow_unsup/007480.png'
-    args.box_name = '/media/yi/DATA/data-orig/kitti/training/label_2/007480.txt'
+    # args.data_path = '/media/yi/DATA/data-orig/kitti/training'
+    # args.image_name = '/media/yi/DATA/data-orig/kitti/training/image_2/007480.png'
+    # args.depth_name = '/media/yi/DATA/data-orig/kitti/training/disp_unsup/007480.png'
+    # args.flow_name = '/media/yi/DATA/data-orig/kitti/training/flow_unsup/007480.png'
+    # args.box_name = '/media/yi/DATA/data-orig/kitti/training/label_2/007480.txt'
 
     if args.data == 'kitti':
         data = KittiData(args.data_path, args.train_proportion, args.test_proportion)
-        # train_meta = data.train_meta
-        # train_data = KittiDataLoader(train_meta, args.batch_size, args.image_heights,
-        #                              args.image_widths, args.output_heights, args.output_widths,
-        #                              args.num_scale, data_augment=True, shuffle=True)
-        # data_test = DataTest(train_data)
-        # data_test.test()
+        train_meta = data.train_meta
+        train_data = KittiDataLoader(train_meta, args.batch_size, args.image_heights,
+                                     args.image_widths, args.output_heights, args.output_widths,
+                                     args.num_scale, data_augment=True, shuffle=True)
+        data_test = DataTest(train_data)
+        data_test.test()
 
-        # test_meta = data.test_meta
-        # test_data = KittiDataLoader(test_meta, args.batch_size, args.image_heights,
-        #                             args.image_widths, args.output_heights, args.output_widths,
-        #                             args.num_scale)
-        # data_test = DataTest(test_data)
-        # data_test.test()
+        test_meta = data.test_meta
+        test_data = KittiDataLoader(test_meta, args.batch_size, args.image_heights,
+                                    args.image_widths, args.output_heights, args.output_widths,
+                                    args.num_scale)
+        data_test = DataTest(test_data)
+        data_test.test()
 
         meta = {'image': [args.image_name], 'depth': [args.depth_name], 'flow': [args.flow_name],
                 'box': [read_box(args.box_name)]}
